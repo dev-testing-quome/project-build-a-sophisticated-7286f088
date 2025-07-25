3955a6808b2d471db0026e4114d9b64808318ce4
jim-schwoebel <jim-schwoebel@users.noreply.github.com>
2025-07-25 00:36:34 +0000

    🎨 Add frontend interface and components

---
+++ frontend/src/pages/ContractListPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContractListPage: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1>Contract List</h1>
+      {/* Add contract list here */}
+    </div>
+  );
+};
+
+export default ContractListPage;
